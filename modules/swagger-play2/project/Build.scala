338dbcdc997e6381d1980467710c399a5f5a3ff5
Paolo Gentili <gentilipaolo@gmail.com>
2013-04-12 10:35:32 +0100

    added exclude of slf4j-log4j as it conflicts with slf4j-logback used in play, deleted repository folder not used anymore

--- modules/swagger-play2/project/Build.scala
+++ modules/swagger-play2/project/Build.scala
@@ -10,7 +10,7 @@ object ApplicationBuild extends Build {
     "com.fasterxml.jackson.module" % "jackson-module-scala" % "2.0.0",
     "com.fasterxml.jackson.core" % "jackson-annotations" % "2.0.0",
     "org.slf4j" % "slf4j-api" % "1.6.4",
-    "com.wordnik" % "swagger-core_2.10.0" % "1.2.0",
+    "com.wordnik" % "swagger-core_2.10.0" % "1.2.0" exclude("org.slf4j", "slf4j-log4j12"),
     "com.wordnik" % "swagger-annotations_2.10.0" % "1.2.0",
     "javax.ws.rs" % "jsr311-api" % "1.1.1")
 
